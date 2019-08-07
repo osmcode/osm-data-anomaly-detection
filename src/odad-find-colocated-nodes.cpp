139e65d73a82f579f2979ed94825b7f04960a0cf
Jochen Topf <jochen@topf.org>
2019-08-07 18:27:33 +0200

    Use close-on-exec flag when opening files to make clang-tidy happy.

--- src/odad-find-colocated-nodes.cpp
+++ src/odad-find-colocated-nodes.cpp
@@ -89,7 +89,7 @@ public:
 
     Bucket(const std::string& dirname, unsigned int n) :
         m_filename(build_filename(dirname, n)),
-        m_fd(::open(m_filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0666)) { // NOLINT(hicpp-signed-bitwise)
+        m_fd(::open(m_filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0666)) { // NOLINT(hicpp-signed-bitwise)
         if (m_fd < 0) {
             throw std::system_error{errno, std::system_category(), std::string{"Can't open file '"} + m_filename + "'"};
         }
@@ -164,7 +164,7 @@ std::vector<osmium::Location> find_locations(const std::string& directory) {
 
     for (unsigned int i = 0; i < num_buckets; ++i) {
         const auto filename = build_filename(directory, i);
-        const int fd = ::open(filename.c_str(), O_RDONLY);
+        const int fd = ::open(filename.c_str(), O_RDONLY | O_CLOEXEC);
         if (fd < 0) {
             throw std::system_error{errno, std::system_category(), std::string{"Can't open file '"} + filename + "'"};
         }
